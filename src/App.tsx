c630973d8a047a293b5fac144c95524ab59f1a02
gabriel1chiro <ichiro6864@gmail.com>
2023-08-31 20:31:20 -0400

    created vite project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="header"></div>
+
+      <h1>Welcome to statistix.io</h1>
+      <div className="card"></div>
+    </React.Fragment>
+  );
+}
+
+export default App;
