dd2041d49287b695c4575f482d319a368e9917d3
gabriel1chiro <ichiro6864@gmail.com>
2023-09-07 10:07:36 -0400

    modified navbar styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,21 +11,23 @@ const Navbar: React.FC = () => {
           <Link to="/">Home</Link>
         </li>
         <li className="navbar-item">
-          <Link to="/documentation">Documentation</Link>
-        </li>
-        <li className="navbar-item">
           <Link to="/contact">Contact or Support</Link>
         </li>
+        {/* dropdown */}
         <li className="navbar-item">
-          <Link to="/about">About</Link>
-        </li>
-        {/* Add a Search Bar */}
-        <li className="navbar-search">
-          <input
-            type="text"
-            placeholder="Search..."
-            // Add event handlers and functionality for search as needed
-          />
+          <div className="dropdown">
+            <button className="dropbtn">
+              Tools
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content">
+              <Link to={`/tools/descriptive-statistics`}>
+                Descriptive Stats
+              </Link>
+              <Link to={`/tools/linear-regression`}>Linear Regression</Link>
+              <Link to={`/tools/moving-averages`}>Moving Averages</Link>
+            </div>
+          </div>
         </li>
       </ul>
     </nav>
